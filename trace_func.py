c6a2253c80b03f54f59fb710067a8e9fd4d1f36c
pogodina-nadezda <nadezda.pogodina.bio@gmail.com>
2019-03-09 21:38:09 +0300

    deleted tracefunc

--- trace_func.py
+++
@@ -1,9 +0,0 @@
-def tracefunc(frame, event, arg):
-    if event == 'return' and frame.f_code.co_name != '_ag':
-        print("function: ", frame.f_code.co_name, ", local vars: ", list(dict.keys(frame.f_locals)))
-    return tracefunc
-
-def tracefoo(function_to_trace):
-    import sys
-    sys.settrace(tracefunc)
-    function_to_trace()
