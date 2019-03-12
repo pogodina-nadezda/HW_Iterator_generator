40398fb9b50e2d93108d181fb68c16ee221f8fd7
pogodina-nadezda <nadezda.pogodina.bio@gmail.com>
2019-03-12 20:39:13 +0300

    changed indentation in generator and added exception in iterator

--- my_range.py
+++ my_range.py
@@ -1,15 +1,18 @@
 class MyRange:
     def __init__(self, start, stop, step = 1):
-        self.start = start
-        self.stop = stop
-        self.step = step
+        if start > stop:
+            raise Exception('There are invalid arguments')
+        else:
+            self.start = start
+            self.stop = stop
+            self.step = step
     
     def __iter__(self):
         return self
     
     def __next__(self):
         while self.start < self.stop:
-            for_print = self.start
+            for_print = self.start        
             self.start += self.step
             return for_print
         else:
@@ -19,6 +22,7 @@ class MyRange:
             
 def MyRangeGenerator(start, stop, step = 1):
     while start < stop:
-            for_print = start
-            start += step
-            yield for_print
+        for_print = start
+        start += step
+        yield for_print
+        
