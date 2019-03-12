5dd9c5b6933fd73c5792e89a1b5d1fcf8f655cc5
pogodina-nadezda <nadezda.pogodina.bio@gmail.com>
2019-03-12 20:39:49 +0300

    added some tests

--- tests/test_my_range_generator.py
+++ tests/test_my_range_generator.py
@@ -13,3 +13,8 @@ class RangeGeneratorTests(unittest.TestCase):
     def test_range(self):
         list_range = [i for i in MyRangeGenerator(-5, 0, 3)]
         self.assertEqual(list_range, [-5, -2])
+    
+    def test_python_range(self):
+        list_my_range = [i for i in MyRangeGenerator(-5, 0, 3)]
+        list_range = [i for i in range(-5, 0, 3)]
+        self.assertEqual(list_my_range, list_range)
--- tests/test_my_range_iterator.py
+++ tests/test_my_range_iterator.py
@@ -12,4 +12,13 @@ class RangeIteratorTests(unittest.TestCase):
     
     def test_range(self):
         list_range = [i for i in MyRange(-5, 0, 3)]
-        self.assertEqual(list_range, [-5, -2], '')
+        self.assertEqual(list_range, [-5, -2])
+    
+    def test_python_range(self):
+        list_my_range = [i for i in MyRange(-5, 0, 3)]
+        list_range = [i for i in range(-5, 0, 3)]
+        self.assertEqual(list_my_range, list_range)
+        
+    def test_wrong_args(self):
+        with self.assertRaises(Exception):
+            MyRange(5,4,1)
